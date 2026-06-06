78ab63a578defa06b0058ba34082224ccaddf306
Lionel Villard <villard@us.ibm.com>
2026-06-06 11:31:43 -0400

    better code reuse
    Signed-off-by: Lionel Villard <villard@us.ibm.com>

--- internal/coordinator/coordinator.go
+++ internal/coordinator/coordinator.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2025 The llm-d Authors
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package coordinator
 
 import (
@@ -93,10 +77,6 @@ func New(c client.Client, plugins []Plugin, opts Options) (*Coordinator, error) 
 		opts:    opts,
 	}, nil
 }
-
-// NeedLeaderElection makes the Coordinator a leader-only manager
-// runnable: non-leaders never enter Start.
-func (c *Coordinator) NeedLeaderElection() bool { return true }
 
 // Start runs the Coordinator loop until ctx is cancelled. It satisfies
 // controller-runtime's manager.Runnable interface.
